bd35732f2f8e5c7a30a4357bbddc2df4ae81a221
loktar00 <loktar69@hotmail.com>
2020-08-05 18:12:07 -0400

    Fixed issues with building

---
+++ src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'core-component-test';
+
+
+
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const SimpleLoader = /*@__PURE__*/createReactComponent<JSX.SimpleLoader, HTMLSimpleLoaderElement>('simple-loader');
